107229c8291cd7c58bddb81c0715af7c913b87df
Tom Stankevich <tstankevich@metamonkey.io>
2024-06-25 10:53:13 -0400

    Move SingletonMonoBehaviour class into CustomTools.Singleton namespace
    The SingletonMonoBehaviour class has been relocated into a new namespace CustomTools.Singleton. This restructuring enhances the organization and references of the class in the larger unity-custom-tools library. No changes were made to the actual functionality or method definitions of the SingletonMonoBehaviour class.

--- Runtime/Singleton/SingletonMonoBehaviour.cs
+++ Runtime/Singleton/SingletonMonoBehaviour.cs
@@ -1,37 +1,40 @@
 using UnityEngine;
 
-public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+namespace CustomTools.Singleton
 {
-    private static T _instance;
-
-    public static T Instance
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
-        {
-            if (_instance != null) 
-                return _instance;
-            
-            _instance = FindObjectOfType<T>();
+        private static T _instance;
 
-            if (_instance != null) 
-                return _instance;
-            
-            GameObject singleton = new GameObject(typeof(T).ToString());
-            _instance = singleton.AddComponent<T>();
-            return _instance;
-        }
-    }
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null)
+                    return _instance;
 
-    protected virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(gameObject);
+                _instance = FindObjectOfType<T>();
+
+                if (_instance != null)
+                    return _instance;
+
+                GameObject singleton = new GameObject(typeof(T).ToString());
+                _instance = singleton.AddComponent<T>();
+                return _instance;
+            }
         }
-        else if (_instance != this)
+
+        protected virtual void Awake()
         {
-            Destroy(gameObject);
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
