5a622ae9e2baac66703eeeebf3487ae4deac8d27
stankispoor <stankispoor@gmail.com>
2026-02-19 14:11:20 -0500

    Update SingletonMonoBehaviour.cs

--- Runtime/Singleton/SingletonMonoBehaviour.cs
+++ Runtime/Singleton/SingletonMonoBehaviour.cs
@@ -6,7 +6,8 @@ namespace CustomTools.Singleton
     {
         public static bool Exists => _instance != null;
         private static T _instance;
-
+        private static bool _isQuitting;
+        
         public static T Instance
         {
             get
@@ -19,6 +20,9 @@ namespace CustomTools.Singleton
                 if (_instance != null)
                     return _instance;
 
+                if (_isQuitting)
+                    return null;
+                
                 GameObject singleton = new GameObject(typeof(T).ToString());
                 _instance = singleton.AddComponent<T>();
                 return _instance;
@@ -36,6 +40,34 @@ namespace CustomTools.Singleton
             {
                 Destroy(gameObject);
             }
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            // During scene unload or quit, Unity destroys objects; treat that as quitting-safe.
+            if (_instance == this)
+                _instance = null;
+        }
+        
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void ResetStatics()
+        {
+            // Important for Domain Reload disabled / Enter Play Mode Options
+            _instance = null;
+            _isQuitting = false;
+
+            Application.quitting -= OnAppQuitting;
+            Application.quitting += OnAppQuitting;
+        }
+        
+        private static void OnAppQuitting()
+        {
+            _isQuitting = true;
+        }
+
+        protected virtual void OnApplicationQuit()
+        {
+            _isQuitting = true;
         }
     }
 }
